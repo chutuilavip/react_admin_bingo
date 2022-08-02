35555454c2d8e54e2b3a49413cb6a3835e4d63be
@htvan <cv.hothanhvan@gmail.com>
2022-08-02 15:30:55 +0700

    history detail

--- src/views/play/tables/Tables.js
+++ src/views/play/tables/Tables.js
@@ -187,21 +187,21 @@ const Tables = () => {
                       <CTableHeaderCell scope="row">{item.total_score}</CTableHeaderCell>
                       <CTableHeaderCell scope="row">{item.logdate}</CTableHeaderCell>
                       <CTableDataCell>
-                        <button>
-                          <svg
-                            onClick={() => handleDetail(item)}
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            height={30}
-                            width={30}
-                            style={{cursor:"pointer"}}
-                          >
-                            <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                          </svg>
-                        </button>
+                        {/* <button> */}
+                        <svg
+                          onClick={() => handleDetail(item)}
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          height={30}
+                          width={30}
+                          style={{ cursor: 'pointer' }}
+                        >
+                          <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                        </svg>
+                        {/* </button> */}
                       </CTableDataCell>
                     </CTableRow>
                   ))}
